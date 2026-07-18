0946682d862cc92cf88fd7cc6a9cb066fa15a1ff
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-07-18 23:02:03 +0530

    added canvas

--- draw.py
+++ draw.py
@@ -1,3 +1,11 @@
 import tkinter as tk
 from PIL import Image, ImageDraw
 
+WIDTH = 280
+HEIGHT = 280
+
+root = tk.Tk()
+root.title("Draw a digit")
+
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="black")
+canvas.pack()
