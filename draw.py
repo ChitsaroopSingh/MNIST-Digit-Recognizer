2051fb323c534aba4f9f1a12e27277d20a31a04a
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-07-19 02:53:09 +0530

    added image to array converter

--- draw.py
+++ draw.py
@@ -1,38 +1,53 @@
 import tkinter as tk
+import numpy as np
 from PIL import Image, ImageDraw
 
 WIDTH = 280
 HEIGHT = 280
 
-root = tk.Tk()
-root.title("Draw a digit")
+def draw_digit():
+    root = tk.Tk()
+    root.title("Draw a digit")
 
-canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="black")
-canvas.pack()
+    canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="black")
+    canvas.pack()
 
-image = Image.new("L", (WIDTH, HEIGHT), 255)
-draw = ImageDraw.Draw(image)
+    image = Image.new("L", (WIDTH, HEIGHT), 0)
+    draw = ImageDraw.Draw(image)
 
-def paint(event):
-    r = 10
+    def paint(event):
+        r = 10
 
-    x1 = event.x - r
-    y1 = event.y - r
-    x2 = event.x + r
-    y2 = event.y + r
+        x1 = event.x - r
+        y1 = event.y - r
+        x2 = event.x + r
+        y2 = event.y + r
 
-    canvas.create_oval(x1, y1, x2 , y2,
-                       fill="white", outline="white")
-    draw.ellipse([x1, y1, x2 , y2], fill=0)
+        canvas.create_oval(x1, y1, x2 , y2,
+                        fill="white", outline="white")
+        draw.ellipse([x1, y1, x2 , y2], fill=255)
 
-canvas.bind("<B1-Motion>", paint)
+    canvas.bind("<B1-Motion>", paint)
 
-def save_image():
-    small = image.resize((28, 28))
-    small.save("digit28.png")
-    print("Saved!")
+    digit = None
+    def save_image():
+        nonlocal digit
+        small = image.resize((28, 28))
+        small.save("digit28.png")
 
-save_button = tk.Button(root, text="Save", command=save_image)
-save_button.pack()
+        digit = np.array(small)
+        digit = digit.reshape(1, 784)
 
-root.mainloop()
+        root.destroy()
+        
+        return digit
+        
+        
+
+    save_button = tk.Button(root, text="Save", command=save_image)
+    save_button.pack()
+
+    root.mainloop()
+
+    return digit
+
