bfbf3b7bcad3ca47a37f912b8b727f0fb8e67f3e
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-07-18 23:14:34 +0530

    feat: drawing ability added and saves drawing

--- draw.py
+++ draw.py
@@ -9,3 +9,30 @@ root.title("Draw a digit")
 
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="black")
 canvas.pack()
+
+image = Image.new("L", (WIDTH, HEIGHT), 255)
+draw = ImageDraw.Draw(image)
+
+def paint(event):
+    r = 10
+
+    x1 = event.x - r
+    y1 = event.y - r
+    x2 = event.x + r
+    y2 = event.y + r
+
+    canvas.create_oval(x1, y1, x2 , y2,
+                       fill="white", outline="white")
+    draw.ellipse([x1, y1, x2 , y2], fill=0)
+
+canvas.bind("<B1-Motion>", paint)
+
+def save_image():
+    small = image.resize((28, 28))
+    small.save("digit28.png")
+    print("Saved!")
+
+save_button = tk.Button(root, text="Save", command=save_image)
+save_button.pack()
+
+root.mainloop()
