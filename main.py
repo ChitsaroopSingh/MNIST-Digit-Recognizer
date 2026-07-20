521584ebc9b54c0044908a06598cd1813720279d
ZeuS <69910617+ChitsaroopSingh@users.noreply.github.com>
2026-07-20 23:28:58 +0530

    added prediction

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import joblib
 from draw import draw_digit
 
+models = {
+    "SGD" : joblib.load("models/sgd_model.joblib"),
+    "Decision Tree" : joblib.load("models/dt_model.joblib"),
+    "KNN" : joblib.load("models/knn_model.joblib"),
+}
+
+digit = draw_digit()
+
+for name, model in models.items():
+    prediction = model.predict(digit)[0]
+    print(f"{name}: {prediction}")
